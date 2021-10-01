a72a4a4e3e909f98ec738d38451a136bb4af8404
Asutosh Pradhan <20ec3014@rgipt.ac.in>
2021-10-01 13:55:55 +0530

    please run this code and check what we are doing

--- main.c
+++ main.c
@@ -1,18 +1,47 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include <windows.h>
 
-
-
-
+void gotoxy(int x, int y)
+{
+  COORD coord;
+  coord.X = x;
+  coord.Y = y;
+  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
 
 void main()
 
-
-
 {
+   
+
+time_t actual = time(NULL);
+time_t duration = 2;
+time_t endwait = actual + duration ;
+printf("WELCOME TO OS ~ SOME\n\n");
+printf("PROJECT BY:\n");
+printf("1.Shivam Sharma(captan)\n");
+printf("2.Asher Ali\n");
+printf("");
+printf("");
+printf("");
+printf("DSG (mentor)\n");
+while(actual < endwait)
+{
+actual = time(NULL);
+
+}
+//now this the main page whwre people will find list of programms
+int i;
+
+system("cls");
+
+	
+	gotoxy(5, 5);
+printf("now this the main page whwre people will find list of programms");
 
 
 
-
-
-
+scanf("%d",&i);
 }
