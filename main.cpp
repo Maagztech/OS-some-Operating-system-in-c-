d278b76c0fed98b2b711509c350df57beff4d90a
Asutosh Pradhan <20ec3014@rgipt.ac.in>
2021-10-02 15:42:37 +0530

    test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include<stdio.h>
-#include<time.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include <windows.h>
 
 using namespace std;
@@ -16,105 +16,113 @@ void gotoxy(int x, int y)
   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-
-
 // the main function starts from here
 int main()
 {
 
-// starting step of welcome and showing of menu page
+  // starting step of welcome and showing of menu page
 
-time_t actual = time(NULL);
-time_t duration = 2;
-time_t endwait = actual + duration ;
-gotoxy(10,10);
-cout << "_*_*_*_*_*_*_*_*_*__*_*_*_*_*WELCOME TO OS-SOME_*_*_*_*_*__*_*_*_*_*_*_\n";
-cout<<"PROJECT BY:\n";
-cout<<"---------------------";
-cout<<"1.Shivam Sharma(captan)\n";
-cout<<"2.Asher Ali\n";
-cout<<"";
-cout<<"";
-cout<<"";
-cout<<"DSG (mentor)\n";
-while(actual < endwait)
-{
-actual = time(NULL);
-}
+  time_t actual = time(NULL);
+  time_t duration = 5;
+  time_t endwait = actual + duration;
+  gotoxy(10, 10);
+  cout << "_*_*_*_*_*_*_*_*_*__*_*_*_*_*WELCOME TO OS-SOME_*_*_*_*_*__*_*_*_*_*_*_\n";
+  cout << "\n\nPROJECT BY:\n";
+  cout << "---------------------\n";
+  cout << "1.Shivam Sharma(captan)\n";
+  cout << "2.Asher Ali\n";
+  cout << "3.Asutosh Pradhan\n";
+  cout << "";
+  cout << "";
+  cout << "DSG (mentor)\n";
+  cout<<"\n\n\n\n\nLoading have patient and read the credits.";
 
-
-
-//now this the main page where people will find list of programms
+char id[12],pass[10];
 system("cls");
+cout<<"::CHOOSE::\n";
+cout<<"-------------\n";
+cout<<"1.login\n";
+cout<<"2.sign-up\n";
+cout<<"3.shutdown\n";
+gotoxy(40,30);
+cout<<"ENTER USER ID:\n";
+cin>>id;
+cout<<"PASSWORD:\n";
+cin>>pass;
 
-		cout << ".................successfuly entered\n" << 
-    "Showing Menu:\n" << "select your choice\n" << "1.Open Applications\n" 
-    << "2.Snake Game\n" << "3.Personal Banking\n";
+  while (actual < endwait)
+  {
+    actual = time(NULL);
+  }
 
-		int choice1; 
-    cout<<"enter your choice";
-    cin >> choice1;
-		if (choice1 == 1) { //just eneterd the appliaction part
-		
-				HANDLE hprocess = NULL; char status = 'y';
-			HANDLE hthread = NULL;
-			STARTUPINFO si;
-			PROCESS_INFORMATION pi;
-			DWORD dwprocessId = 0;
-			DWORD dwthreadId = 0;
-			ZeroMemory(&si, sizeof(si));
-			ZeroMemory(&pi, sizeof(pi));
-			cout << "1. Batch's Projects" << "\t" << "2. Windows Media Player" << "\t" << "3.Notepad\n";
-			int cia; cin >> cia; //choice in apps
-			if (cia == 2) {
-				BOOL bCreateProcess = NULL;
+  //now this the main page where people will find list of programms
+  system("cls");
 
-				bCreateProcess = CreateProcess(L"C:\\Program Files\\Windows Media Player\\wmplayer.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-				if (bCreateProcess == FALSE)
-				{
-					cout << "couldnotrocess\n" << GetLastError() << endl;
-				}
-				cout << "program launched succesfully\n" << endl;
-				cout << "processid" << pi.dwProcessId << endl;
-				cout << "threadid" << pi.dwThreadId << endl;
-				cout << "getprocessid" << GetProcessId(pi.hProcess) << endl;
-				WaitForSingleObject(pi.hProcess, INFINITE);
-				CloseHandle(pi.hThread);
-				CloseHandle(pi.hProcess);
-			}//end of wmplayer 
-			if (cia == 3) {
-				BOOL cCreateprocess = NULL;
-				cCreateprocess = CreateProcess(L"C:\\Windows\\notepad.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-				WaitForSingleObject(pi.hProcess, INFINITE);
-				CloseHandle(pi.hThread);
-				CloseHandle(pi.hProcess);
-			} //end of notepad
-			if (cia == 1) { // while(status2!=0){
-				cout << " we are working on this feature";
-				/*
-			while(status2!=0) {}cout<< thhis feature allows you to open batch's programs easily BOOL dcreateprocess = NULL;
-				 dcreateprocess = CreateProcess(L"", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-				 WaitForSingleObject(pi.hProcess, INFINITE);
-				 CloseHandle(pi.hThread);
-				 CloseHandle(pi.hProcess);
-				 cout<<"1 to continue opening programs and 0 to exit"; cin>>status2 }*/
+  cout << ".................successfuly entered\n"
+       << "Showing Menu:\n"
+       << "select your choice\n"
+       << "1.Open Applications of other groups\n"
+       << "2.WORD (*recommended to try)\n"
+       << "3.NOTEPAD\n"
+       << "4.GAMES"
+       <<"5.VIDEO PLAYER"
+       <<"YOUTUBE";
 
 
-			} // end of batch's prgm
-			cout << "enter 1 to continue with apps and 0 to exit"; cin >> status1; //end of apps
-		}  cout << "enter 1 to main menu and 0 to exit"; cin >> status; //end of menu
-	}
-}
-				/* if (n == 3) {
-					 BOOL ecreateprocess = NULL;
-					 ecreateprocess = CreateProcess(L"", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-					 WaitForSingleObject(pi.hProcess, INFINITE);
-					 CloseHandle(pi.hThread);
-					 CloseHandle(pi.hProcess);
-				 }*/
 
 
-int i;
-scanf("%d",&i);
-return 0;
+
+
+
+
+
+
+
+
+  // int choice1;
+  // cout << "enter your choice :-";
+  // cin >> choice1;
+  // if (choice1 == 1)
+  // {
+  //   //just eneterd the appliaction part
+
+  //   cout << "1. Batch's Projects"
+  //        << "\t"
+  //        << "2. Windows Media Player"
+  //        << "\t"
+  //        << "3.Notepad\n";
+
+  //   int cia;
+  //   cin >> cia; //choice in apps
+  // }
+
+  // // only tried to open
+  // HANDLE hprocess = NULL;
+  // char status = 'y';
+  // HANDLE hthread = NULL;
+  // STARTUPINFO si;
+  // PROCESS_INFORMATION pi;
+  // DWORD dwprocessId = 0;
+  // DWORD dwthreadId = 0;
+  // ZeroMemory(&si, sizeof(si));
+  // ZeroMemory(&pi, sizeof(pi));
+  // BOOL bCreateProcess = NULL;
+
+  // bCreateProcess = CreateProcess(L"C:\\Windows\\notepad.exe", NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+  // if (bCreateProcess == FALSE)
+  // {
+  //   cout << "couldnotrocess\n"
+  //        << GetLastError() << endl;
+  // }
+  // cout << "program launched succesfully\n"
+  //      << endl;
+  // cout << "processid" << pi.dwProcessId << endl;
+  // cout << "threadid" << pi.dwThreadId << endl;
+
+  // WaitForSingleObject(pi.hProcess, INFINITE);
+  // CloseHandle(pi.hThread);
+  // CloseHandle(pi.hProcess);
+int y;
+cin>>y;
+  return (0);
 }
